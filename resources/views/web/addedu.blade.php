f47e529ca90b051f4d541787739727d216d1aa27
zulkawsar <zul.kawsar@gmail.com>
2021-03-09 21:46:58 +0600

    board

--- resources/views/web/addedu.blade.php
+++ resources/views/web/addedu.blade.php
@@ -1,26 +1,197 @@
 <tr>
     <th>
     	<select class="form-control" name="exam[]">
-    	    <option>Choose</option>
-    	    <option>Option 1</option>
-    	    <option>Option 2</option>
-    	    <option>Option 3</option>
+    	    <option value="">Choose</option>
+    	    <option value="ssc">SSC</option>
+    	    <option value="hsc">HSc</option>
+    	    <option value="bsc">Bsc</option>
     	</select>
     </th>
     <td>
     	<select class="form-control" name="university[]">
-    	    <option>Choose</option>
-    	    <option>Option 1</option>
-    	    <option>Option 2</option>
-    	    <option>Option 3</option>
+    	    <option value="">Choose</option>
+            <option value="Bangabandhu Sheikh Mujib Medical University">Bangabandhu Sheikh Mujib Medical University</option>
+            <option value="Bangabandhu Sheikh Mujibur Rahman Agricultural University">Bangabandhu Sheikh Mujibur Rahman Agricultural University</option>
+            <option value="Bangladesh Agricultural University,Mymensingh">Bangladesh Agricultural University,Mymensingh</option>
+            <option value="Bangladesh Open University">Bangladesh Open University</option>
+            <option value="Bangladesh University of Engineering &amp; Technology">Bangladesh University of Engineering &amp; Technology</option>
+            <option value="Bangladesh University of Professionals">Bangladesh University of Professionals</option>
+            <option value="Chittagong University of Engineering &amp; Technology">Chittagong University of Engineering &amp; Technology</option>
+            <option value="Chittagong Veterinary and Animal Sciences University">Chittagong Veterinary and Animal Sciences University</option>
+            <option value="Comilla University">Comilla University</option>
+            <option value="Dhaka University">Dhaka University</option>
+            <option value="Dhaka University of Engineering &amp; Technology">Dhaka University of Engineering &amp; Technology</option>
+            <option value="Hajee Mohammad Danesh Science &amp; Technology University">Hajee Mohammad Danesh Science &amp; Technology University</option>
+            <option value="Islamic University, Kushtia ">Islamic University, Kushtia </option>
+            <option value="Jagannath University">Jagannath University</option>
+            <option value="Jahangirnagar University">Jahangirnagar University</option>
+            <option value="Jatiya Kabi Kazi Nazrul Islam University">Jatiya Kabi Kazi Nazrul Islam University</option>
+            <option value="Jessore Science &amp; Technology University">Jessore Science &amp; Technology University</option>
+            <option value="Khulna University">Khulna University</option>
+            <option value="Khulna University of Engineering and Technology">Khulna University of Engineering and Technology</option>
+            <option value="Mawlana Bhashani Science &amp; Technology University">Mawlana Bhashani Science &amp; Technology University</option>
+            <option value="National University">National University</option>
+            <option value="Noakhali Science &amp; Technology University">Noakhali Science &amp; Technology University</option>
+            <option value="Pabna University of Science and Technology">Pabna University of Science and Technology</option>
+            <option value="Patuakhali Science And Technology University">Patuakhali Science And Technology University</option>
+            <option value="Rajshahi University">Rajshahi University</option>
+            <option value="Rajshahi University of Engineering &amp; Technology">Rajshahi University of Engineering &amp; Technology</option>
+            <option value="Rangpur University">Rangpur University</option>
+            <option value="Shahjalal University of Science &amp; Technology">Shahjalal University of Science &amp; Technology</option>
+            <option value="Sher-e-Bangla Agricultural University">Sher-e-Bangla Agricultural University</option>
+            <option value="Sylhet Agricultural University">Sylhet Agricultural University</option>
+            <option value="University of Chittagong">University of Chittagong</option>
+            <option value="Ahsanullah University of Science and Technology">Ahsanullah University of Science and Technology</option>
+            <option value="America Bangladesh University">America Bangladesh University</option>
+            <option value="American International University Bangladesh">American International University Bangladesh</option>
+            <option value="ASA University Bangladesh">ASA University Bangladesh</option>
+            <option value="Asian University of Bangladesh">Asian University of Bangladesh</option>
+            <option value="Atish Dipankar University of Science &amp; Technology">Atish Dipankar University of Science &amp; Technology</option>
+            <option value="Bangladesh Islami University">Bangladesh Islami University</option>
+            <option value="Bangladesh University">Bangladesh University</option>
+            <option value="Bangladesh University of Business &amp; Technology (BUBT)">Bangladesh University of Business &amp; Technology (BUBT)</option>
+            <option value="BGC Trust University Bangladesh, Chittagong">BGC Trust University Bangladesh, Chittagong</option>
+            <option value="BRAC University">BRAC University</option>
+            <option value="Central Women's University">Central Women's University</option>
+            <option value="City University">City University</option>
+            <option value="Daffodil International University">Daffodil International University</option>
+            <option value="Darul Ihsan University">Darul Ihsan University</option>
+            <option value="Dhaka International University">Dhaka International University</option>
+            <option value="East Delta University , Chittagong">East Delta University , Chittagong</option>
+            <option value="East West University">East West University</option>
+            <option value="Eastern University">Eastern University</option>
+            <option value="Gono Bishwabidyalay">Gono Bishwabidyalay</option>
+            <option value="Green University of Bangladesh">Green University of Bangladesh</option>
+            <option value="IBAIS University">IBAIS University</option>
+            <option value="Independent University, Bangladesh">Independent University, Bangladesh</option>
+            <option value="International Islamic University, Chittagong">International Islamic University, Chittagong</option>
+            <option value="International University of Business Agriculture &amp; Technology">International University of Business Agriculture &amp; Technology</option>
+            <option value="Leading University, Sylhet">Leading University, Sylhet</option>
+            <option value="Manarat International University">Manarat International University</option>
+            <option value="Metropolitan University, Sylhet">Metropolitan University, Sylhet</option>
+            <option value="North South University">North South University</option>
+            <option value="Northern University Bangladesh">Northern University Bangladesh</option>
+            <option value="Premier University, Chittagong">Premier University, Chittagong</option>
+            <option value="Presidency University">Presidency University</option>
+            <option value="Prime University">Prime University</option>
+            <option value="Primeasia University">Primeasia University</option>
+            <option value="Queens University">Queens University</option>
+            <option value="Royal University of Dhaka">Royal University of Dhaka</option>
+            <option value="Shanto Mariam University of Creative Technology">Shanto Mariam University of Creative Technology</option>
+            <option value="Southeast University">Southeast University</option>
+            <option value="Southern University of Bangladesh , Chittagong">Southern University of Bangladesh , Chittagong</option>
+            <option value="Stamford University, Bangladesh">Stamford University, Bangladesh</option>
+            <option value="State University Of Bangladesh">State University Of Bangladesh</option>
+            <option value="Sylhet International University, Sylhet">Sylhet International University, Sylhet</option>
+            <option value="The Millenium University">The Millenium University</option>
+            <option value="The Peoples University of Bangladesh">The Peoples University of Bangladesh</option>
+            <option value="The University of Asia Pacific">The University of Asia Pacific</option>
+            <option value="United International University">United International University</option>
+            <option value="University of Development Alternative">University of Development Alternative</option>
+            <option value="University of Information Technology &amp; Sciences">University of Information Technology &amp; Sciences</option>
+            <option value="University of Liberal Arts Bangladesh">University of Liberal Arts Bangladesh</option>
+            <option value="University of Science &amp; Technology, Chittagong">University of Science &amp; Technology, Chittagong</option>
+            <option value="University of South Asia">University of South Asia</option>
+            <option value="Uttara University">Uttara University</option>
+            <option value="Victoria University of Bangladesh">Victoria University of Bangladesh</option>
+            <option value="World University of Bangladesh">World University of Bangladesh</option>
+            <option value="Asian University for Women">Asian University for Women</option>
+            <option value="Islamic University of Technology">Islamic University of Technology</option>
+            <option value="South Asian University">South Asian University</option>
+            <option value="Dhaka Medical College">Dhaka Medical College</option>
+            <option value="Sir Salimullah Medical College">Sir Salimullah Medical College</option>
+            <option value="Mymensingh Medical College">Mymensingh Medical College</option>
+            <option value="Chittagong Medical College">Chittagong Medical College</option>
+            <option value="Rajshahi Medical College">Rajshahi Medical College</option>
+            <option value="MAG Osmani Medical College">MAG Osmani Medical College</option>
+            <option value="Sher-E-Bangla Medical College">Sher-E-Bangla Medical College</option>
+            <option value="Rangpur Medical College">Rangpur Medical College</option>
+            <option value="Comilla Medical College">Comilla Medical College</option>
+            <option value="Khulna Medical College">Khulna Medical College</option>
+            <option value="Shaheed Ziaur Rahman Medical College">Shaheed Ziaur Rahman Medical College</option>
+            <option value="Dinajpur Medical College">Dinajpur Medical College</option>
+            <option value="Faridpur Medical College">Faridpur Medical College</option>
+            <option value="Shaheed Suhrawardy Medical College">Shaheed Suhrawardy Medical College</option>
+            <option value="Pabna Medical College">Pabna Medical College</option>
+            <option value="Noakhali Medical College">Noakhali Medical College</option>
+            <option value="Cox`s Bazar Medical College">Cox`s Bazar Medical College</option>
+            <option value="Jessore Medical College">Jessore Medical College</option>
+            <option value="Shaheed Nazrul Islam Medical College">Shaheed Nazrul Islam Medical College</option>
+            <option value="Kushtia Medical College">Kushtia Medical College</option>
+            <option value="Satkhira Medical College">Satkhira Medical College</option>
+            <option value="Sheikh Sayera Khatun Medical College, Gopalganj">Sheikh Sayera Khatun Medical College, Gopalganj</option>
+            <option value="Feni Medical College,Feni">Feni Medical College,Feni</option>
+            <option value="Gono Bishwabidyalay, Savar, Dhaka">Gono Bishwabidyalay, Savar, Dhaka</option>
+            <option value="Ad-din Womens Medical College, Dhaka">Ad-din Womens Medical College, Dhaka</option>
+            <option value="Anwer Khan Modern Medical College, Dhaka">Anwer Khan Modern Medical College, Dhaka</option>
+            <option value="Bangladesh Medical College">Bangladesh Medical College</option>
+            <option value="Jalalabad Rageb-Rabeya Medical College,Sylhet">Jalalabad Rageb-Rabeya Medical College,Sylhet</option>
+            <option value="BGC Trust Medical College, Chittagong">BGC Trust Medical College, Chittagong</option>
+            <option value="Central Medical College, Comilla">Central Medical College, Comilla</option>
+            <option value="Chottagram Ma-O-Shishu Hospital Medical College">Chottagram Ma-O-Shishu Hospital Medical College</option>
+            <option value="Community Based Medical College (cbmc), Mymensingh">Community Based Medical College (cbmc), Mymensingh</option>
+            <option value="Community Medical College, Dhaka">Community Medical College, Dhaka</option>
+            <option value="Delta Medical College, Dhaka">Delta Medical College, Dhaka</option>
+            <option value="Dhaka National Medical College">Dhaka National Medical College</option>
+            <option value="Durra Samad Rahman Red Crescent Women�s Medical College, Sylhet">Durra Samad Rahman Red Crescent Women�s Medical College, Sylhet</option>
+            <option value="Eastern Medical College, Comilla">Eastern Medical College, Comilla</option>
+            <option value="Enam Medical College, Savar, Dhaka">Enam Medical College, Savar, Dhaka</option>
+            <option value="Sylhet Women`s Medical College, Sylhet">Sylhet Women`s Medical College, Sylhet</option>
+            <option value="Green Life Medical College,Dhaka">Green Life Medical College,Dhaka</option>
+            <option value="Holy Family Red Crescent Medical College, Dhaka">Holy Family Red Crescent Medical College, Dhaka</option>
+            <option value="Ibrahim Medical College, Dhaka">Ibrahim Medical College, Dhaka</option>
+            <option value="Ibn Sina Medical College, Dhaka">Ibn Sina Medical College, Dhaka</option>
+            <option value="International Medical College, Gazipur">International Medical College, Gazipur</option>
+            <option value="Islami Bank Medical College, Rajshahi">Islami Bank Medical College, Rajshahi</option>
+            <option value="Jahurul Islam Medical College, Kishoregonj">Jahurul Islam Medical College, Kishoregonj</option>
+            <option value="Jalalabad Ragib-Rabeya Medical College Sylhet">Jalalabad Ragib-Rabeya Medical College Sylhet</option>
+            <option value="Khawja Yunus Ali Medical College, Sirajganj">Khawja Yunus Ali Medical College, Sirajganj</option>
+            <option value="Kumudini Medical College, Tangail">Kumudini Medical College, Tangail</option>
+            <option value="Labaid Medical College[6] Dhanmondi, Dhaka">Labaid Medical College[6] Dhanmondi, Dhaka</option>
+            <option value="Maulana Bhasani Medical College">Maulana Bhasani Medical College</option>
+            <option value="Medical College for Women and Hospital, Dhaka">Medical College for Women and Hospital, Dhaka</option>
+            <option value="Nightingale Medical College, Dhaka">Nightingale Medical College, Dhaka</option>
+            <option value="North Bengal Medical College, Sirajganj">North Bengal Medical College, Sirajganj</option>
+            <option value="North East Medical College, Sylhet">North East Medical College, Sylhet</option>
+            <option value="Northern International Medical College, Dhaka">Northern International Medical College, Dhaka</option>
+            <option value="Northern Private Medical College, Rangpur">Northern Private Medical College, Rangpur</option>
+            <option value="Popular Medical College &amp; Hospital, Dhaka">Popular Medical College &amp; Hospital, Dhaka</option>
+            <option value="Prime Medical College, Rangpur">Prime Medical College, Rangpur</option>
+            <option value="Rangpur Community Hospital Medical College">Rangpur Community Hospital Medical College</option>
+            <option value="Sahabuddin Medical College and Hospital">Sahabuddin Medical College and Hospital</option>
+            <option value="Samaj Vittik Medical College, Mirzanagar, Savar">Samaj Vittik Medical College, Mirzanagar, Savar</option>
+            <option value="Shahabuddin Medical College, Dhaka">Shahabuddin Medical College, Dhaka</option>
+            <option value="Z. H. Sikder Women`s Medical College">Z. H. Sikder Women`s Medical College</option>
+            <option value="Southern Medical College, Chittagong">Southern Medical College, Chittagong</option>
+            <option value="Tairunnessa Memorial Medical College, Gazipur">Tairunnessa Memorial Medical College, Gazipur</option>
+            <option value="TMSS Medical College,Bogra">TMSS Medical College,Bogra</option>
+            <option value="University Of Science and Technology Chittagong. IAMS">University Of Science and Technology Chittagong. IAMS</option>
+            <option value="Uttara Adhunik Medical College,Dhaka">Uttara Adhunik Medical College,Dhaka</option>
+            <option value="Military Institute of Science and Technology (MIST)">Military Institute of Science and Technology (MIST)</option>
+            <option value="Others">Others</option>
+            <option value="Sonargaon University">Sonargaon University</option>
+            <option value="North Bengal International University">North Bengal International University</option>
+            <option value="Begum Rokeya University">Begum Rokeya University</option>
+            <option value="Bangladesh Army University of Science and Technology">Bangladesh Army University of Science and Technology</option>
     	</select>
     </td>
     <td>
     	<select class="form-control" name="board[]">
-    	    <option>Choose</option>
-    	    <option>Option 1</option>
-    	    <option>Option 2</option>
-    	    <option>Option 3</option>
+    	    <option value="">Choose</option>
+    	    <option value="Dhaka">Dhaka</option>
+            <option value="Cumilla">Cumilla</option>
+            <option value="Rajshahi">Rajshahi</option>
+            <option value="Jashore">Jashore</option>
+            <option value="Chittagong">Chittagong</option>
+            <option value="Barishal">Barishal</option>
+            <option value="Sylhet">Sylhet</option>
+            <option value="Dinajpur">Dinajpur</option>
+            <option value="Madrasah">Madrasah</option>
+            <option value="Technical">Technical</option>
+            <option value="Cambridge International - IGCE">Cambridge International - IGCE</option>
+            <option value="Edexcel International">Edexcel International</option>
+            <option value="Bangladesh Technical Education Board (BTEB)">Bangladesh Technical Education Board (BTEB)</option>
+            <option value="Others">Others</option>
     	</select>
     </td>
     <td>
